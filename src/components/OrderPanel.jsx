62d2d40afc4f31c46ae18a42e482974f480c163c
honglee666 <76101246+honglee666@users.noreply.github.com>
2025-04-24 18:07:45 +0800

    Update OrderPanel.jsx

--- src/components/OrderPanel.jsx
+++ src/components/OrderPanel.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-function OrderPanel() {
+function OrderPanel({ onOrder }) {
   return (
-    <div>
-      <button style={{ marginRight: '1rem' }}>做多</button>
-      <button>做空</button>
+    <div style={{ marginBottom: '1rem' }}>
+      <button onClick={() => onOrder('buy')} style={{ marginRight: '1rem' }}>做多</button>
+      <button onClick={() => onOrder('sell')}>做空</button>
     </div>
   );
 }
